a254c8d3259b81009676fad21703f9fdadda0238
eze-kiel <ezekiel@fastmail.com>
2020-06-04 18:59:42 +0200

    added prod flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"time"
@@ -9,11 +10,25 @@ import (
 )
 
 func main() {
-	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: handlers.Handle(),
+	var prod bool
+
+	flag.BoolVar(&prod, "prod", false, "production mode")
+	flag.Parse()
+
+	if prod {
+		srv := &http.Server{
+			Addr:    ":80",
+			Handler: handlers.Handle(),
+		}
+		fmt.Printf("[PROD | %v] : Serving...\n", time.Now().Format("Mon Jan 2 15:04:05"))
+		srv.ListenAndServe()
+	} else {
+		srv := &http.Server{
+			Addr:    ":8080",
+			Handler: handlers.Handle(),
+		}
+		fmt.Printf("DEV | [%v] : Serving...\n", time.Now().Format("Mon Jan 2 15:04:05"))
+		srv.ListenAndServe()
 	}
 
-	fmt.Printf("[%v] : Serving...\n", time.Now().Format("Mon Jan 2 15:04:05"))
-	srv.ListenAndServe()
 }
